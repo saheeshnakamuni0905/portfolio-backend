af66da61c23ac16126dd040637c9d071e20cfdc1
saheeshnakamuni0905 <kamunisaheeshna5@gmail.com>
2025-04-17 23:14:42 -0400

    backend code

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+
+// ==== Email Validation Route (Abstract API) ====
+app.post('/api/validate-email', async (req, res) => {
+  const { email } = req.body;
+  const apiKey = process.env.ABSTRACT_API_KEY;
+  try {
+    const response = await axios.get('https://emailvalidation.abstractapi.com/v1/', {
+      params: {
+        api_key: apiKey,
+        email: email.trim(), 
+      }
+    });
+
+    const { is_valid_format, is_disposable_email } = response.data;
+
+    if (!is_valid_format.value || is_disposable_email.value) {
+      return res.status(400).json({ error: 'Invalid or disposable email.' });
+    }
+
+    res.json({ valid: true });
+  } catch (error) {
+    console.error("Validation error:", error.message);
+    res.status(500).json({ error: 'Email validation failed.' });
+  }
+});
+
+// const frontendPath = path.join(__dirname, '../frontend/build/');
+// console.log(frontendPath);
+// app.use(express.static(frontendPath));
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
